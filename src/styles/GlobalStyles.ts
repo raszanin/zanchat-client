68c6d879dd578bc6ec1531db49187d00944b88ce
Roberto Zanin <raszanin@gmail.com>
2023-03-21 13:51:14 -0300

    GlobalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+import RobotoRegular from '../assets/fonts/Roboto-Regular.ttf';
+import RobotoMedium from '../assets/fonts/Roboto-Medium.ttf';
+import RobotoBold from '../assets/fonts/Roboto-Bold.ttf';
+
+export const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: 'Roboto';
+  font-weight: 400;
+  font-display: 'swap';
+  font-style: 'normal';
+  src: url('${RobotoRegular}') format('truetype');
+}
+@font-face {
+  font-family: 'Roboto';
+  font-weight: 500;
+  font-display: 'swap';
+  font-style: 'normal';
+  src: url('${RobotoMedium}') format('truetype');
+}
+@font-face {
+  font-family: 'Roboto';
+  font-weight: 700;
+  font-display: 'swap';
+  font-style: 'normal';
+  src: url('${RobotoBold}') format('truetype');
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Roboto, sans-serif;
+}
+body {
+  background: #fafafa;
+  color: #000;
+}
+button {
+  cursor: pointer;
+  font-size: 1rem;
+  color: #f2f2f2;
+}
+`;
