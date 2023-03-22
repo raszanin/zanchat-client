6919f82772f06600dbe7ed4994008a8491cc1507
Roberto Zanin <raszanin@gmail.com>
2023-03-22 13:39:07 -0300

    https req

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:3333',
-  baseURL: 'http://zanchat-server.onrender.com',
+  baseURL: 'https://zanchat-server.onrender.com',
 });
 
 export default api;
